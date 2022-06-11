1832cf3a450916b37aab1288e58de91f518074e7
Wojtasko99 <damianero90@gmail.com>
2022-06-11 23:34:17 +0200

    poprawny

--- mongo/import.sh
+++ mongo/import.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-mongoimport --db projekt --collection forms --type csv --file nowe_dane.csv --headerline
+mongoimport --host=127.0.0.1 --db projekt --collection forms --type csv --file nowe_dane.csv --headerline
